023e800786e17618d794dde1b82fd7f6cc4f0fae
audisiogaston <audisiogaston@gmail.com>
2023-06-10 10:44:27 -0300

    agrego dto,fatory

--- Desafio-Autenticacion de Github/src/service/factory.js
+++ Desafio-Autenticacion de Github/src/service/factory.js
@@ -4,7 +4,7 @@ import MongoSingleton from '../config/mongodb-singleton.js';
 // Para cambiar de persistencia
 // node src/app.js --persist files --mode dev
 
-let studentService
+let userService
 
 switch (config.persistence) {
     case 'mongodb':
@@ -18,20 +18,20 @@ switch (config.persistence) {
             }
         };
         mongoInstance();
-        const { default: StudentServiceMongo } = await import('./db/user.service.js')
-        studentService = new StudentServiceMongo();
-        console.log("Student service loaded:");
-        console.log(studentService);
+        const { default: UserServiceMongo } = await import('./db/user.service.js')
+        userService = new UserServiceMongo();
+        console.log("User service loaded:");
+        console.log(userService);
         break;
     case 'files':
-        const { default: StudentServiceFileSystem } = await import('./fileSystem/user.service.js')
-        studentService = new StudentServiceFileSystem();
-        console.log("Student service loaded:");
-        console.log(studentService);
+        const { default: UserServiceFileSystem } = await import('./fileSystem/user.service.js')
+        userService = new UserServiceFileSystem();
+        console.log("User service loaded:");
+        console.log(userService);
         break;
 
     default:
         break;
 }
 
-export { studentService }
+export { userService }
