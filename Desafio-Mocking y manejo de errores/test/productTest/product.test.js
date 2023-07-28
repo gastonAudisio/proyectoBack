e9cbf21b0c4ba21eda1ffdd79608eb438fa00005
audisiogaston <audisiogaston@gmail.com>
2023-07-27 21:37:07 -0300

    agrego testProduct

---
+++ Desafio-Mocking y manejo de errores/test/productTest/product.test.js
@@ -0,0 +1,31 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from "../../src/app.js";
+
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('Product API', () => {
+  // Test para la ruta GET /api/products
+  describe('GET /api/products', () => {
+    
+    it('Debería devolver todos los productos', (done) => {
+      chai
+        .request(app)
+        .get('/api/products')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.an('array');
+          done();
+        });
+    }).timeout(5000); 
+  });
+
+  // Otros tests para otros métodos y rutas de productRouter
+  // describe('POST /api/products', () => { ... });
+  // describe('GET /api/products/:id', () => { ... });
+  // describe('PUT /api/products/:id', () => { ... });
+  // describe('DELETE /api/products/:id', () => { ... });
+});
+
