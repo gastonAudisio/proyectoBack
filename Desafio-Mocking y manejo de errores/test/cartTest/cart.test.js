766766f979a4e3283ebce819f643f3804bee1c5b
audisiogaston <audisiogaston@gmail.com>
2023-07-30 19:43:04 -0300

    agrego cartTest

--- Desafio-Mocking y manejo de errores/test/cartTest/cart.test.js
+++ Desafio-Mocking y manejo de errores/test/cartTest/cart.test.js
@@ -7,20 +7,19 @@ const requester = supertest("http://localhost:9090/api/carts");
 
 describe('Test router Cart', function () {
 
-    before(function () {
-        this.cartId = null;
-        this.productIdInCart = null;
-    });
-
-    it('Se debe crear un cart', async function () {
+    it('Debe crear un cart', async function () {
         const { statusCode, body } = await requester.post('/');
         expect(statusCode).to.be.ok;
         expect(body).to.have.a.property('_id');
-         // Capturar el ID del carrito creado
         this.cartId = body._id;
         console.log('Carrito creado con ID:', this.cartId);
     });
-  
+
+    it('Debe eliminar el cart', async function () {
+        const { statusCode, body } = await requester.delete('/');
+        expect(statusCode).to.be.ok;
+    });
+
 
 });
 
